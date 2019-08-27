946447ea7f6f86dc0f18ec909c8110c9e17e95f2
heini-phys-chem <heini.phys.chem@gmail.com>
2019-08-27 13:20:53 +0200

    my own pyscf calculator

---
+++ pyscf_simple.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python
+
+import numpy as np
+from ase.calculators.calculator import Calculator
+import ase.dft.kpoints
+from ase.lattice import bulk
+
+from pyscf import gto, scf, grad, mp
+
+def ase_atoms_to_pyscf(ase_atoms):
+  '''Convert ASE atoms to PySCF atom.
+
+  Note: ASE atoms always use A.
+  '''
+#return [[ase_atoms.get_chemical_symbols(), ase_atoms.get_positions()] for i, atom in enumerate(ase_atoms)]
+  return [ [ase_atoms.get_chemical_symbols()[i], ase_atoms.get_positions()[i]] for i in range(len(ase_atoms.get_positions()))]
+
+class PySCF_simple(Calculator):
+	name = 'PySCF_simple'
+	implemented_properties = ['energies', 'force']
+
+	def __init__(self, atoms, method, basis):
+		self.mol = gto.M(verbose=0)
+		self.mol.atom = ase_atoms_to_pyscf(atoms)
+		self.mol.basis = basis
+		self.mol.build()
+
+		self.method = method
+
+
+	def get_potential_energy(self, atoms=None, force_consistent=False):
+		if self.method != 'MP2':
+			self.mf = scf.RHF(self.mol)
+			energy = self.mf.kernel()
+
+			return energy
+
+		else:
+			self.mf  = scf.RHF(self.mol)
+			hf_energy = self.mf.kernel()
+
+			self.mp2 = mp.MP2(self.mf)
+			corr_energy   = self.mp2.kernel()
+
+			return corr_energy[0] + hf_energy 
+
+
+	def get_forces(self, atoms=None):
+		if self.method != 'MP2':
+			forces  = -1*grad.RHF(self.mf).kernel()
+
+			return forces
+
+		else:
+			self.mf  = scf.RHF(self.mol)
+			self.mf.kernel()
+
+			self.mp2 = mp.MP2(self.mf)
+			self.mp2.kernel()
+
+			forces = -1*grad.mp2.Grad(self.mp2).kernel()
+
+			return forces
